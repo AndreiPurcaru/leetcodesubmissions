24d28964a656bf500647eedfc31636c96cb6d5db
Andrei Purcaru <andrei.purcaru@teampicnic.com>
2026-04-16 11:41:14 +0200

    Start LeetCode Quest for refresher

---
+++ kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+    println(buildArray(intArrayOf(1,3), 3))
+}
+
+fun buildArray(target: IntArray, n: Int): List<String> {
+    val seq = generateSequence(0) {it + 1}.iterator()
+    val result = ArrayDeque<StackOperation>()
+
+    for (currentIndex in target.indices) {
+        var currentSeqNumber = seq.next()
+        while (currentSeqNumber < target[currentIndex] - 1) {
+            result.add(StackOperation.PUSH)
+            result.add(StackOperation.POP)
+            currentSeqNumber = seq.next()
+        }
+        if (currentSeqNumber < target[currentIndex]) {
+            result.add(StackOperation.PUSH)
+        }
+        if (currentSeqNumber >= n) {
+            break
+        }
+    }
+    return result.toList().map {it.toString()}
+}
+
+enum class StackOperation() {
+    PUSH, POP;
+
+    override fun toString(): String {
+        return name.lowercase().replaceFirstChar { it.uppercase() }
+    }
+}
+
+
